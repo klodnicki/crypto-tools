62cf7b399a8a98add55a2758ef0140c689e3d2ea
Josh Klodnicki <joshklod@gmail.com>
2021-05-23 21:35:46 -0400

    Remove HTTP server for now

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 #!/usr/bin/env node
 
-const express = require('express');
-const app = express();
-const port = 7963;
-
-app.get('/', (req, res) => {
-	res.send('Hello, World!');
-});
-
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
