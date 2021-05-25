eebe845a1276f7fe9e84b499ee90927513466548
Josh Klodnicki <joshklod@gmail.com>
2021-05-24 21:56:36 -0400

    Add JSON data test

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
-const ccxt   = require('ccxt');
+process.on('unhandledRejection', e => console.error(e));
+
+const ccxt       = require('ccxt');
+const jsonfile   = require('jsonfile');
 
 console.log(ccxt.exchanges.filter(s => s.match('binance')));
+
+jsonfile.readFile('./testData.json')
+	.catch(err => {
+		if (err.code == 'ENOENT') return {};
+		else                      throw err;
+	})
+	.then(console.dir);
