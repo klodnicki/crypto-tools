1fd1611441324e169842dc0387c52d9e64093f94
Josh Klodnicki <joshklod@gmail.com>
2021-05-25 22:08:57 -0400

    Add private exchange data test

--- index.js
+++ index.js
@@ -7,9 +7,37 @@ const jsonfile   = require('jsonfile');
 
 console.log(ccxt.exchanges.filter(s => s.match('binance')));
 
-jsonfile.readFile('./testData.json')
+const xt = {};
+
+jsonfile.readFile('./data.json')
 	.catch(err => {
 		if (err.code == 'ENOENT') return {};
 		else                      throw err;
 	})
-	.then(console.dir);
+	.then(data => {
+		for (const [username, user] of Object.entries(data.users)) {
+			xt[username] = {};
+			for (const [xtName, xtData] of Object.entries(user.exchanges)) {
+				xt[username][xtName] =
+					new ccxt[xtData.exchange](xtData.credentials);
+
+				if (process.env.NODE_ENV !== 'production')
+					xt[username][xtName].setSandboxMode(true);
+
+				try {
+					xt[username][xtName].checkRequiredCredentials();
+				} catch (e) {
+					console.warn(Object.assign(e, {
+						user:       username,
+						exchange:   xtName
+					}));
+					delete xt[username][xtName];
+					continue;
+				}
+
+				xt[username][xtName].fetchBalance()
+					.then(d => d.total)
+					.then(console.dir);
+			}
+		}
+	});
